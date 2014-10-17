4a55383b534bb48a10cf4b9a7fc168c123e2be9c
Jacob Kingery <jacob.kingery@students.olin.edu>
2014-10-17 15:11:02 -0400

    began work on clock

--- clock.py
+++ clock.py
@@ -2,6 +2,15 @@
 class Clock (object):
 
     def __init__ (self,time):
-        pass
+        self._registry = {}
+        self._time = time
 
-    # FIX ME
+    def register (self, f, priority):
+        if self._registry[priority]:
+            self._registry[priority].append(f)
+        else:
+            self._registry[priority] = [f]
+
+    def tick (self):
+        self._time += 1
+        # Go through all functions in registry by priority
